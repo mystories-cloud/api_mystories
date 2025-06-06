b3577d63535f6737547c733cbdad67eb1cccb56a
Azkar97 <muhammadazkar359@gmail.com>
2025-06-06 19:11:23 +0500

    changed to string

--- app/Services/SyncToGA.php
+++ app/Services/SyncToGA.php
@@ -63,7 +63,7 @@ class SyncToGA
                         'name' => 'page_view', // Using the standard GA4 event name for page views
                         'params' => [
                             'items' => [
-                                'test_item' => 1,
+                                'test_item' => '1',
                             ],
                             'page_location' => 'https://mystories.cloud',
                             'page_referrer' => '', // Populate this if you have referrer data in your PageView model
